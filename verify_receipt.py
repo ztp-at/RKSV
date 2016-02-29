fb5d87f396927ce28d7b11f8a6f44cc87588f3b8
Mino Sharkhawy <mino@ztp.at>
2016-02-29 13:42:51 +0100

    Allow usage of a key store with ReceiptVerifier

--- verify_receipt.py
+++ verify_receipt.py
@@ -9,6 +9,10 @@ class CertSerialMismatchException(rechnung.ReceiptException):
     def __init__(self, receipt):
         super(CertSerialMismatchException, self).__init__(receipt, "Certificate serial mismatch.")
 
+class NoPublicKeyException(rechnung.ReceiptException):
+    def __init__(self, receipt):
+        super(NoPublicKeyException, self).__init__(receipt, "No public key found.")
+
 class InvalidSignatureException(rechnung.ReceiptException):
     def __init__(self, receipt):
         super(InvalidSignatureException, self).__init__(receipt, "Invalid Signature.")
@@ -25,15 +29,19 @@ class ReceiptVerifierI:
         raise NotImplementedError("Please implement this yourself.")
 
 class ReceiptVerifier(ReceiptVerifierI):
-    def __init__(self, keyStore):
+    def __init__(self, keyStore, cert):
         self.keyStore = keyStore
+        self.cert = cert
 
     @staticmethod
     def fromDEPCert(depCert):
-        keyStore = key_store.KeyStore()
-        keyStore.putPEMCert(utils.addPEMCertHeaders(depCert))
+        cert = utils.loadCert(utils.addPEMCertHeaders(depCert))
 
-        return ReceiptVerifier(keyStore)
+        return ReceiptVerifier(None, cert)
+
+    @staticmethod
+    def fromKeyStore(keyStore):
+        return ReceiptVerifier(keyStore, None)
 
     def verify(self, receipt, algorithmPrefix):
         jwsString = receipt.toJWSString(algorithmPrefix)
@@ -47,9 +55,17 @@ class ReceiptVerifier(ReceiptVerifierI):
         if certSerial[0] == '-':
             certSerial = certSerial[1:]
 
-        pubKey = self.keyStore.getKey(certSerial)
+        # TODO: validate form of serial/keyId
+
+        pubKey = None
+        if self.cert:
+            # TODO: if cert, validate that cert serials match
+            pubKey = self.cert.public_key()
+        else:
+            pubKey = self.keyStore.getKey(certSerial)
+
         if not pubKey:
-            raise CertSerialMismatchException(jwsString)
+            raise NoPublicKeyException(jwsString)
 
         validationSuccessful = algorithm.verify(jwsString, pubKey)
 
@@ -71,6 +87,7 @@ class ReceiptVerifier(ReceiptVerifierI):
 
         return self.verify(receipt, algorithmPrefix)
 
+import configparser
 import sys
 
 INPUT_FORMATS = {
@@ -80,7 +97,7 @@ INPUT_FORMATS = {
 
 if __name__ == "__main__":
     if len(sys.argv) < 3 or len(sys.argv) > 4:
-        print("Usage: ./verify_receipt.py <format> <cert file> [<receipt string>]")
+        print("Usage: ./verify_receipt.py <format> <key store> [<receipt string>]")
         sys.exit(0)
 
     if sys.argv[1] not in INPUT_FORMATS:
@@ -88,10 +105,10 @@ if __name__ == "__main__":
         sys.exit(0)
 
     rv = None
-    with open(sys.argv[2]) as f:
-        keyStore = key_store.KeyStore()
-        keyStore.putPEMCert(f.read())
-        rv = ReceiptVerifier(keyStore)
+    config = configparser.RawConfigParser()
+    config.read(sys.argv[2])
+    keyStore = key_store.KeyStore.readStore(config)
+    rv = ReceiptVerifier.fromKeyStore(keyStore)
 
     if len(sys.argv) == 4:
         INPUT_FORMATS[sys.argv[1]](rv, sys.argv[3])
